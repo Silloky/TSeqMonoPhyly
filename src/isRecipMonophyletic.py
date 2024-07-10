74e1a8a899ab6f6c9e4af01316a00ba07801bdac
Elias Kirkwood <94617006+Silloky@users.noreply.github.com>
2024-07-10 14:21:13 +0000

    Refac : commented and improved monophyletic test

---
+++ src/isRecipMonophyletic.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import tskit as tsk
+
+def isRecipMonophyletic(tree, pop_by_node) :
+
+    nodes_by_pop = pd.DataFrame(columns=['nodes']) # This will be the list of leaves per population
+
+    for leaf in tree.leaves(tree.root):
+
+        current_pop = pop_by_node.loc[leaf, 'pop'] # Get the current leaf's population
+
+        if current_pop != 2 : # Forget ancestral nodes
+            if (current_pop not in nodes_by_pop.index):
+                nodes_by_pop.loc[current_pop] = [[]] # Create a new entry for the population
+
+            nodes_by_pop.at[current_pop, 'nodes'].append(leaf) # Add the leaf to the population
+    
+    nodes_by_pop['mrca'] = nodes_by_pop['nodes'].apply(lambda x: tree.mrca(*x)) # Identify the MRCA of the population's leaves
+
+    nodes_by_pop['toplevelChildren'] = nodes_by_pop['mrca'].apply(lambda x: list(tree.leaves(x))) # Identify the MRCA's children
+
+    # If each population's MRCA's children are the same as the population's leaves, all populations are reciprocally monophyletic
+
+    return (nodes_by_pop['nodes'] == nodes_by_pop['toplevelChildren']).all()
