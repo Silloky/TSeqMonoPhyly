09de0191b189ac8b028616337ec5c02ffaf99456
Elias Kirkwood <94617006+Silloky@users.noreply.github.com>
2024-07-12 08:00:45 +0000

    Fix : adapt code for real data & improve color coding function

--- src/isRecipMonophyletic.py
+++ src/isRecipMonophyletic.py
@@ -1,19 +1,20 @@
 import pandas as pd
 import tskit as tsk
 
-def isRecipMonophyletic(tree, pop_by_node) :
+def isRecipMonophyletic(tree, pop_by_node, ancestral) :
 
     nodes_by_pop = pd.DataFrame(columns=['nodes']) # This will be the list of leaves per population
 
-    for leaf in tree.leaves(tree.root):
+    for root in tree.roots:
+        for leaf in tree.leaves(root):
 
-        current_pop = pop_by_node.loc[leaf, 'pop'] # Get the current leaf's population
+            current_pop = pop_by_node.loc[leaf, 'pop'] # Get the current leaf's population
 
-        if current_pop != 2 : # Forget ancestral nodes
-            if (current_pop not in nodes_by_pop.index):
-                nodes_by_pop.loc[current_pop] = [[]] # Create a new entry for the population
+            if current_pop != ancestral : # Forget ancestral nodes
+                if (current_pop not in nodes_by_pop.index):
+                    nodes_by_pop.loc[current_pop] = [[]] # Create a new entry for the population
 
-            nodes_by_pop.at[current_pop, 'nodes'].append(leaf) # Add the leaf to the population
+                nodes_by_pop.at[current_pop, 'nodes'].append(leaf) # Add the leaf to the population
     
     nodes_by_pop['mrca'] = nodes_by_pop['nodes'].apply(lambda x: tree.mrca(*x)) # Identify the MRCA of the population's leaves
 
