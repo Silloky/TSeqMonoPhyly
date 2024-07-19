5aafbcf47bcd940344bcca72b2c6b6ce5cb04020
Silloky (Elias K.) <ek.mindstorms@outlook.com>
2024-07-19 14:36:07 +0200

    Fix : working main script (pop groups, !ancestral) & Feat: allow bootstrap bypass
    Co-authored-by: PA-GAGNAIRE <PA-GAGNAIRE@users.noreply.github.com>

--- src/isRecipMonophyletic.py
+++ src/isRecipMonophyletic.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import tskit as tsk
 
-def isRecipMonophyletic(tree, pop_by_node, ancestral, pop_groups) :
+def isRecipMonophyletic(tree, pop_by_node, pop_groups) :
 
     nodes_by_pop = pd.DataFrame(columns=['nodes']) # This will be the list of leaves per population
 
@@ -10,19 +10,18 @@ def isRecipMonophyletic(tree, pop_by_node, ancestral, pop_groups) :
 
             current_pop = pop_by_node.loc[leaf, 'pop'] # Get the current leaf's population
 
-            if current_pop != ancestral : # Forget ancestral nodes
-                if (current_pop not in nodes_by_pop.index):
-                    nodes_by_pop.loc[current_pop] = [[]] # Create a new entry for the population
+            if (current_pop not in nodes_by_pop.index):
+                nodes_by_pop.loc[current_pop] = [set()] # Create a new entry for the population
 
-                nodes_by_pop.at[current_pop, 'nodes'].append(leaf) # Add the leaf to the population
+            nodes_by_pop.at[current_pop, 'nodes'].add(leaf) # Add the leaf to the population
 
     for pop_group in pop_groups:
-        nodes_by_pop.loc[pop_group[0], 'nodes'] = nodes_by_pop.loc[pop_group[0], 'nodes'] + nodes_by_pop.loc[pop_group[1], 'nodes']
+        nodes_by_pop.loc[pop_group[0], 'nodes'] = nodes_by_pop.loc[pop_group[0], 'nodes'].union(nodes_by_pop.loc[pop_group[1], 'nodes'])
         nodes_by_pop.drop(pop_group[1], inplace=True)
-    
+
     nodes_by_pop['mrca'] = nodes_by_pop['nodes'].apply(lambda x: tree.mrca(*x)) # Identify the MRCA of the population's leaves
 
-    nodes_by_pop['toplevelChildren'] = nodes_by_pop['mrca'].apply(lambda x: list(tree.leaves(x))) # Identify the MRCA's children
+    nodes_by_pop['toplevelChildren'] = nodes_by_pop['mrca'].apply(lambda x: set(tree.leaves(x))) # Identify the MRCA's children
 
     # If each population's MRCA's children are the same as the population's leaves, all populations are reciprocally monophyletic
 
